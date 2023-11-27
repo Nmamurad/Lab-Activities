0e6d5f032477ddc4518da082c89bdd7cc564f819
Nma <147346261+Nmamurad@users.noreply.github.com>
2023-11-27 13:35:16 +0300

    Daily-Activity

--- src/Lab1.java
+++ src/Lab1.java
@@ -1,6 +1,51 @@
+import java.util.Scanner;
 public class Lab1 {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    Scanner input= new Scanner(System.in);
+
+     private  String name;
+      private Boolean value;
+      private int number;
+
+
+
+    public Lab1(int number) {
+        this.number = number;
+    }
+    public Lab1 (String Name, int Number){
+        this.name=Name;
+        this.number=Number;
+
+    }
+public Lab1(String Name, int Number, Boolean Value){
+    this.name=Name;
+    this.number=Number;
+    this.value=Value;
+}
+    public int getNumber() {
+        return number;
     }
 
+    public Boolean getValue() {
+        return value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+
+public void Display(){
+        System.out.println("The Informations are:");
+        System.out.println(" Number:"+getNumber()+"\n"+
+                            "Name: "+getName()+"\n"+
+                             "Value:"+getValue());
+
+
+
+
 }
+}
+        ;
+
+
